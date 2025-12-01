2a7f0865171bf77204c1a91d526fb1589685bbbb
O-Mercy <cheria.mercy@gmail.com>
2025-12-01 15:00:40 -0600

    Part 4 complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,10 +10,12 @@ public class Main {
         System.out.println("What do you want to do?");
         System.out.println("1) Display all products");
         System.out.println("2) Display all customers");
+        System.out.println("3) Display all categories");
         System.out.println("0) Exit");
         System.out.println("Select an option: ");
 
         int choice = scanner.nextInt();
+        scanner.nextLine();
 
         String url = "jdbc:mysql://localhost:3306/northwind";
         String user = "root";
@@ -62,6 +64,42 @@ public class Main {
                     System.out.println("Country: " + country);
                     System.out.println("Country: " + phone);
                 }
+            }
+            else if (choice == 3){
+                Statement statement = connection.createStatement();
+                String query = "SELECT categoryID, categoryName FROM categories ORDER BY categoryID";
+
+                ResultSet results = statement.executeQuery(query);
+
+                while (results.next()){
+                    String categoryID = results.getString("categoryID");
+                    String categoryName = results.getString("CategoryName");
+
+                    System.out.println("Category ID: " + categoryID);
+                    System.out.println("Category Name: " + categoryName);
+                }
+
+                System.out.println("Select Category ID: ");
+                String input = scanner.nextLine();
+
+                query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products WHERE categoryID = ?";
+
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, input);
+                ResultSet filteredList = preparedStatement.executeQuery();
+
+
+                while (filteredList.next()) {
+                    String productID = filteredList.getString("ProductID");
+                    String productName = filteredList.getString("ProductName");
+                    String unitPrice = filteredList.getString("UnitPrice");
+                    String unitsInStock = filteredList.getString("UnitsInStock");
+                    System.out.println("Product Id: " + productID);
+                    System.out.println("Product Name: " + productName);
+                    System.out.println("Product Unit Price: " + unitPrice);
+                    System.out.println("Product Units in Stock: " + unitsInStock);
+                }
+
             } else if (choice == 0) {
                 System.out.println("Goodbye!");
             }
