9e35d4013fdbb039cde12f263690bc68315220b0
O-Mercy <cheria.mercy@gmail.com>
2025-12-01 13:56:26 -0600

    Part 3 complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,78 @@
 import java.sql.Connection;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException{
-        Connection connection;
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup");
+    public static void main(String[] args) {
 
-        Statement statement = connection.createStatement();
+        Scanner scanner = new Scanner(System.in);
 
-        String query = """
-                SELECT ProductID, ProductName, UnitPrice, UnitsInStock
-                FROM products""";
+        System.out.println("What do you want to do?");
+        System.out.println("1) Display all products");
+        System.out.println("2) Display all customers");
+        System.out.println("0) Exit");
+        System.out.println("Select an option: ");
 
-        ResultSet results = statement.executeQuery(query);
+        int choice = scanner.nextInt();
 
-        while (results.next()) {
-            String product = results.getString("ProductID");
-            System.out.println(product);
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        String user = "root";
+        String password = "yearup";
+
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+
+            if (choice == 1) {
+                Statement statement = connection.createStatement();
+
+                String query = """
+                        SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+                        FROM products""";
+
+                ResultSet results = statement.executeQuery(query);
+
+                while (results.next()) {
+                    String productID = results.getString("ProductID");
+                    String productName = results.getString("ProductName");
+                    String unitPrice = results.getString("UnitPrice");
+                    String unitsInStock = results.getString("UnitsInStock");
+                    System.out.println("Product Id: " + productID);
+                    System.out.println("Product Name: " + productName);
+                    System.out.println("Product Unit Price: " + unitPrice);
+                    System.out.println("Product Units in Stock: " + unitsInStock);
+                }
+            } else if (choice == 2) {
+                Statement statement = connection.createStatement();
+
+                String query = """
+                        SELECT ContactName, CompanyName, City, Country, Phone
+                        FROM customers ORDER BY Country""";
+
+                ResultSet results = statement.executeQuery(query);
+
+                while (results.next()) {
+                    String contactName = results.getString("ContactName");
+                    String companyName = results.getString("CompanyName");
+                    String city = results.getString("City");
+                    String country = results.getString("Country");
+                    String phone = results.getString("Phone");
+                    System.out.println("Contact: " + contactName);
+                    System.out.println("Company: " + companyName);
+                    System.out.println("City: " + city);
+                    System.out.println("Country: " + country);
+                    System.out.println("Country: " + phone);
+                }
+            } else if (choice == 0) {
+                System.out.println("Goodbye!");
+            }
+
+            else {
+                System.out.println("Invalid option.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
         }
-        connection.close();
-
     }
 }
+
